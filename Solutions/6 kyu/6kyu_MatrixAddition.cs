a1b7acd47c72d770ecc89519e8d5a68d02b70906
secundoGeminae <because_of_u@mail.ru>
2018-04-03 14:15:12 +0300

    Create 6kyu_MatrixAddition.cs
    Writing a function which accepts two square matrices (nxn two dimensional arrays), and return the sum of the two.

---
+++ Solutions/6 kyu/6kyu_MatrixAddition.cs
@@ -0,0 +1,15 @@
+using System;
+public class Kata
+{
+  public static int[][] MatrixAddition(int[][] a, int[][] b)
+  {
+     for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < a[i].Length; j++)
+                {
+                    a[i][j] = a[i][j] + b[i][j];
+                }
+            }
+    return a;
+  }
+}
