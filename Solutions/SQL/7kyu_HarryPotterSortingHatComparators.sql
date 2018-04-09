b0dfa4956c384773d3c49257e3975dbedff7de3e
secundoGeminae <because_of_u@mail.ru>
2018-04-09 15:57:01 +0300

    Create 7kyu_HarryPotterSortingHatComparators.sql

---
+++ Solutions/SQL/7kyu_HarryPotterSortingHatComparators.sql
@@ -0,0 +1,7 @@
+Select * 
+from students 
+where ((quality1='evil' and quality2='cunning')
+or (quality1='brave' and quality2 !='evil')
+or  (quality1='studious' or quality2='intelligent')
+or (quality1='hufflepuff' or quality2='hufflepuff'))
+order by id
